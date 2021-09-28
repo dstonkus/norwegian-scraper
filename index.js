1fb014477705b0e3cf2618f00c15353c90b6ab58
dstonkus <stonkusdd@gmail.com>
2021-09-29 01:11:51 +0300

    final

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const args = {
 
 const url = `https://www.norwegian.com/api/fare-calendar/calendar?adultCount=1&destinationAirportCode=${args.arrivalAirport}&originAirportCode=${args.departureAirport}&outboundDate=2021-11-01&tripType=1&currencyCode=EUR&languageCode=en-GB&pageId=258774&eventType=user`;
 
-// puppeteer usage as normal
+//////////
 puppeteer.launch({ headless: true }).then(async (browser) => {
   console.log('Running scraper..');
   const page = await browser.newPage();
@@ -25,22 +25,23 @@ puppeteer.launch({ headless: true }).then(async (browser) => {
   // gets the dates and prices
   for (let i = 3; i < 33; i++) {
     try {
+      // date selector
       date = await page.$eval(
         `#folder${i} > div.opened > div:nth-child(1) > span:nth-child(2)`,
         (el) =>
           el.textContent
             .trim()
-
             .replaceAll(':', '')
             .replaceAll('T', '')
             .substring(0, 10)
       );
-
+      // price selector
       price = await page.$eval(
         `#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`,
         (el) => el.textContent
       );
 
+      // gets the day from date
       singleDate = await date.substr(-2);
 
       dayObj.push({
@@ -60,11 +61,11 @@ puppeteer.launch({ headless: true }).then(async (browser) => {
 
   // goes to page and scrapes data
   for (let i = 0; i < priceAndDate.length; i++) {
-    let dataUrl = `https://www.norwegian.com/en/ipc/availability/avaday?D_City=OSL&A_City=FCO&TripType=1&D_Day=${dayObj[i].day}&D_Month=202111&D_SelectedDay=${dayObj[i].day}&R_Day=${dayObj[i].day}&R_Month=202111&R_SelectedDay=${dayObj[i].day}&dFlight=DY1872OSLFCO&dCabinFareType=1&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=EUR&ShowNoFlights=True&rnd=58814&processid=94015`;
+    const dataUrl = `https://www.norwegian.com/en/ipc/availability/avaday?D_City=OSL&A_City=FCO&TripType=1&D_Day=${dayObj[i].day}&D_Month=202111&D_SelectedDay=${dayObj[i].day}&R_Day=${dayObj[i].day}&R_Month=202111&R_SelectedDay=${dayObj[i].day}&dFlight=DY1872OSLFCO&dCabinFareType=1&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=EUR&ShowNoFlights=True&rnd=58814&processid=94015`;
 
     await page.goto(dataUrl);
 
-    let selector =
+    const selector =
       '#avaday-outbound-result > div > div > div.bodybox > div > table > tbody';
 
     departureAirport = await page.$eval(
